03f49f5684898d9095048c6cd1de73536a49eaff
Alessio2021 <alessiobut1911@gmail.com>
2022-02-16 14:20:59 +0100

    classes

---
+++ classes/User.php
@@ -0,0 +1,117 @@
+<?php
+
+class User
+{
+    protected $name;
+    protected $lastname;
+    protected $mail;
+    protected $city;
+    protected $country;
+
+    public function __construct($name, $lastname, $mail)
+    {
+        $this->name = $name;
+        $this->lastname = $lastname;
+        $this->mail = $mail;
+    }
+
+    /**
+     * Get the value of country
+     */ 
+    public function getCountry()
+    {
+        return $this->country;
+    }
+
+    /**
+     * Set the value of country
+     *
+     * @return  self
+     */ 
+    public function setCountry($country)
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of city
+     */ 
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * Set the value of city
+     *
+     * @return  self
+     */ 
+    public function setCity($city)
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of mail
+     */ 
+    public function getMail()
+    {
+        return $this->mail;
+    }
+
+    /**
+     * Set the value of mail
+     *
+     * @return  self
+     */ 
+    public function setMail($mail)
+    {
+        $this->mail = $mail;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of lastname
+     */ 
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * Set the value of lastname
+     *
+     * @return  self
+     */ 
+    public function setLastname($lastname)
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of name
+     */ 
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */ 
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once __DIR__ . '/classes/Product.php';
+require_once __DIR__ . '/classes/User.php';
+
+$user = new User('Pippo', 'Pluto', 'pippo.pluto@mail.com');
+// var_dump($user);
